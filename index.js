acc3cc2f8e024d33385f171de56e5559bc8be058
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-05 16:14:18 -0500

    changed question arrays ad added prompt functions

--- index.js
+++ index.js
@@ -9,36 +9,159 @@ const Intern = require('./lib/Intern');
 
 // array will be needed to put the answers into
 const generatedTeamArray = [];
-
+const managerQuestionsArray = [
+  {
+    type: 'input',
+    message: 'What is the team manager\'s name?',
+    name: 'name',
+  },
+  {
+    type: 'input',
+    message: 'What is the team manager\'s employee ID',
+    name: 'employeeId',
+  },
+  {
+    type: 'input',
+    message: 'What is the team manager\'s email address?',
+    name: 'emailAddress',
+  },
+  {
+    type: 'input',
+    message: 'What is the team manger\'s Office Number',
+    name: 'officeNumber',
+  },
+]
+const newMemberQuestion = [
+  {
+    type: 'list',
+    message: 'Which type of meamber would you like to add to your team?',
+    name: 'role',
+    choices: ['Enngineer', 'Intern', 'Finished']
+  }
+]
 // inquirer questions 
+// questions for Manager on initial start
+/* 
 const questions = async() => {
   const managerAnswers = await inquirer
     .prompt([
       {
         type: 'input',
-        message: 'What is your Name?',
+        message: 'What is the team manager\'s name?',
         name: 'name',
       },
       {
         type: 'input',
-        message: 'What is your employee ID',
+        message: 'What is the team manager\'s employee ID',
         name: 'employeeId',
       },
       {
         type: 'input',
-        message: 'What is your email address?',
+        message: 'What is the team manager\'s email address?',
         name: 'emailAddress',
       },
       {
         type: 'input',
-        message: 'What is your Office Number',
+        message: 'What is the team manger\'s Office Number',
         name: 'officeNumber',
       },
+    ])
+    
+    // questions for Engineer
+    if (managerAnswers.role === 'Engineer'){
+      const engineerAnswers = await inquirer
+      .prompt([
+        {
+          type: 'input',
+          message: 'What is your engineer\'s name?',
+          name: 'name',
+        },
+        {
+          type: 'input',
+          message: 'What is your engineer\'s employee ID',
+          name: 'employeeId',
+        },
+        {
+          type: 'input',
+          message: 'What is your engineer\'s email address?',
+          name: 'emailAddress',
+        },
+        {
+          type: 'input',
+          message: 'What is your engineer\'s GitHub username?',
+          name: 'github',
+        },
+      ])
+    }
+    // questions for Intern
+    if (managerAnswers.role === 'Intern'){
+      const internAnswers = await inquirer
+      .prompt([
+        {
+          type: 'input',
+          message: 'What is your intern\'s name?',
+          name: 'name',
+        },
+        {
+          type: 'input',
+          message: 'What is your inern\'s employee ID',
+          name: 'employeeId',
+        },
+        {
+          type: 'input',
+          message: 'What is your intern\'s email address?',
+          name: 'emailAddress',
+        },
+        {
+          type: 'input',
+          message: 'What school does your intern attend?',
+          name: 'school',
+        },
+      ])
+    }
+    console.log(managerAnswers);
+  }
+
+  //funciton to ask member questions outside of manager questions
+  async function promptMemberQuestions(){
+    await questions
+    
+    const promptNewMemberAdd = await inquirer
+    .prompt([
       {
         type: 'list',
-        message: 'Who woudl you like to add to your team?',
+        message: 'Which type of meamber would you like to add to your team?',
         name: 'role',
         choices: ['Enngineer', 'Intern', 'Finished']
       },
     ])
+  }
+  */
+function promptMemberQuestions(){
+  inquirer.prompt(newMemberQuestion)
+  .then((answers) =>{
+    console.log(answers);
+  })
+  .catch((error) => {
+  console.log(error);
+  })
 }
+
+// initialize the application
+function init() {
+  inquirer.prompt(managerQuestionsArray)
+    .then((answers) =>{
+        console.log(answers);
+      promptMemberQuestions()
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+}
+    
+    // will need a function to generate the HTML and write to file
+    function generateTeam(){
+      
+}
+
+ init()
