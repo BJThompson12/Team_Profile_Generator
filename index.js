7050c0fc002fcccddd7d1a1e4fd7143a32ae17f1
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-05 21:20:42 -0500

    created functions for prompting questions and storing answers to an array

--- index.js
+++ index.js
@@ -36,7 +36,51 @@ const newMemberQuestion = [
     type: 'list',
     message: 'Which type of meamber would you like to add to your team?',
     name: 'role',
-    choices: ['Enngineer', 'Intern', 'Finished']
+    choices: ['Engineer', 'Intern', 'Finished']
+  }
+]
+const engineerQuestionsArray = [
+  {
+    type: 'input',
+    message: 'What is your engineer\'s name?',
+    name: 'name',
+  },
+  {
+    type: 'input',
+    message: 'What is your engineer\'s employee ID',
+    name: 'employeeId',
+  },
+  {
+    type: 'input',
+    message: 'What is your engineer\'s email address?',
+    name: 'emailAddress',
+  },
+  {
+    type: 'input',
+    message: 'What is your engineer\'s GitHub username?',
+    name: 'github',
+  },
+]
+const internQuestionsArray = [
+  {
+    type: 'input',
+    message: 'What is your intern\'s name?',
+    name: 'name',
+  },
+  {
+    type: 'input',
+    message: 'What is your inern\'s employee ID',
+    name: 'employeeId',
+  },
+  {
+    type: 'input',
+    message: 'What is your intern\'s email address?',
+    name: 'emailAddress',
+  },
+  {
+    type: 'input',
+    message: 'What school does your intern attend?',
+    name: 'school',
   }
 ]
 // inquirer questions 
@@ -141,17 +185,57 @@ function promptMemberQuestions(){
   inquirer.prompt(newMemberQuestion)
   .then((answers) =>{
     console.log(answers);
+    if (answers.role === 'Engineer') {
+      promptEngineerQuestions();
+    }
+    else if (answers.role === 'Intern') {
+      promptInternQuestions();
+    }
+    else if (answers.role === 'Finished'){
+      console.log(generatedTeamArray);
+      generateTeam();
+      return;
+    }
   })
   .catch((error) => {
   console.log(error);
   })
 }
 
+// function to prompt engineer questions
+function promptEngineerQuestions(){
+  inquirer.prompt(engineerQuestionsArray)
+  .then((answers) =>{
+    answers['role']= 'Manager';
+    console.log(answers);
+    promptMemberQuestions();
+  })
+  .catch((error) => {
+  console.log(error);
+  })
+}
+
+//function to prompt intern questions
+function promptInternQuestions(){
+  inquirer.prompt(internQuestionsArray)
+  .then((answers) =>{
+    answers['role']= 'Intern';
+    console.log(answers);
+    generatedTeamArray.push(answers)
+    console.log(generatedTeamArray);
+    promptMemberQuestions();
+  })
+  .catch((error) => {
+  console.log(error);
+  })
+}
 // initialize the application
 function init() {
   inquirer.prompt(managerQuestionsArray)
     .then((answers) =>{
-        console.log(answers);
+      answers['role']= 'Manager';
+      console.log(answers);
+      generatedTeamArray.push(answers)
       promptMemberQuestions()
     })
     .catch((error) => {
@@ -165,3 +249,9 @@ function init() {
 }
 
  init()
+
+ // looping array to generate the html
+ for (let peopleObj of generatedTeamArray){
+  let name = peopleObj.name
+  let employeeId = peopleObj.employeeId
+ }
