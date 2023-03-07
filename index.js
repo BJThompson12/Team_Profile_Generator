456c6b67331a4fa5a1cf2cdaa8340b9585ac1558
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-03-07 16:26:37 -0500

    added al lquestions and prompts.

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const managerQuestionsPrompt = require('./lib/managerQuestions')
+const engineerQuestionsPrompt = require('./lib/engineerQuestions')
+const internQuestionsPrompt = require('./lib/internQuestions')
 
 // array will be needed to put the answers into
 const generatedTeamArray = [];
 
-const newMemberQuestion = [
+const newTeamMemberQuestion = [
   {
     type: 'list',
     message: 'Which type of meamber would you like to add to your team?',
@@ -19,50 +21,8 @@ const newMemberQuestion = [
     choices: ['Engineer', 'Intern', 'Finished']
   }
 ]
-const engineerQuestionsArray = [
-  {
-    type: 'input',
-    message: 'What is your engineer\'s name?',
-    name: 'name',
-  },
-  {
-    type: 'input',
-    message: 'What is your engineer\'s employee ID',
-    name: 'employeeId',
-  },
-  {
-    type: 'input',
-    message: 'What is your engineer\'s email address?',
-    name: 'emailAddress',
-  },
-  {
-    type: 'input',
-    message: 'What is your engineer\'s GitHub username?',
-    name: 'github',
-  },
-]
-const internQuestionsArray = [
-  {
-    type: 'input',
-    message: 'What is your intern\'s name?',
-    name: 'name',
-  },
-  {
-    type: 'input',
-    message: 'What is your inern\'s employee ID',
-    name: 'employeeId',
-  },
-  {
-    type: 'input',
-    message: 'What is your intern\'s email address?',
-    name: 'emailAddress',
-  },
-  {
-    type: 'input',
-    message: 'What school does your intern attend?',
-    name: 'school',
-  }
-]
+
+
 
 //check prompt inputs using validate method from inquirer
 const validateInput = (userInput) => {
@@ -71,58 +31,6 @@ const validateInput = (userInput) => {
     return "No value entered. Please enter a value.";
   } else return true;
 }
-// inquirer questions 
-// questions for Manager on initial start
-/*function promptMemberQuestions(){
-  inquirer.prompt(newMemberQuestion)
-  .then((answers) =>{
-    console.log(answers);
-    if (answers.role === 'Engineer') {
-      promptEngineerQuestions();
-    }
-    else if (answers.role === 'Intern') {
-      promptInternQuestions();
-    }
-    else if (answers.role === 'Finished'){
-      console.log(generatedTeamArray);
-      generateTeam();
-      return;
-    }
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-}
-*/
-// function to prompt engineer questions
-function promptEngineerQuestions(){
-  inquirer.prompt(engineerQuestionsArray)
-  .then((answers) =>{
-    // add the role to the object
-    answers['role']= 'Manager';
-    console.log(answers);
-    promptMemberQuestions();
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-}
-
-//function to prompt intern questions
-function promptInternQuestions(){
-  inquirer.prompt(internQuestionsArray)
-  .then((answers) =>{
-    answers['role']= 'Intern';
-    console.log(answers);
-    generatedTeamArray.push(answers)
-    console.log(generatedTeamArray);
-    promptMemberQuestions();
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-}
-
 // initialize the application
 const init = async () => {
   await inquirer.prompt([
@@ -133,49 +41,110 @@ const init = async () => {
       type: 'confirm',
     }
   ])
-  console.log(this);
   if (!this) {
     return;
   } else {
     // start manager questions with a prompt
-   inquirer.prompt(managerQuestionsPrompt)
-   .then((managerAnswers) => {
-    const teamManager = new Manager(managerAnswers.name, managerAnswers.employeeId, managerAnswers.emailAddress, managerAnswers.officeNumber);
-    console.log(teamManager);
-     generatedTeamArray.push(teamManager)
-     console.log(generatedTeamArray);
-   })
+    inquirer.prompt(managerQuestionsPrompt)
+    .then((answers) => {
+      const teamManager = new Manager(answers.name, answers.employeeId, answers.emailAddress, answers.officeNumber);
+      console.log(teamManager);
+      generatedTeamArray.push(teamManager)
+      console.log(generatedTeamArray);
+      addTeamMemeber();
+    })
+  };
+};
+
+const addTeamMemeber = () => {
+  inquirer.prompt(newTeamMemberQuestion)
+  .then((answer) =>{
+    if (answer.role === 'None'){
+      return;
+    } else if (answer.role === 'Engineer') {
+      inquirer.prompt(engineerQuestionsPrompt)
+      .then((answers) => {
+        const EngineerAnswers = new Engineer(answers.name, answers.employeeId, answers.emailAddress, answers.github);
+        console.log(EngineerAnswers);
+        addTeamMemeber();
+      })
+    } else if (answer.role === 'Intern') {
+      inquirer.prompt(internQuestionsPrompt)
+      .then((answers) => {
+        const InternAnswers = new Intern(answers.name, answers.employeeId, answers.emailAddress, answers.school);
+        console.log(InternAnswers);
+        addTeamMemeber();
+      })
+    }
+  })
+};
+  // will need a function to generate the HTML and write to file
+  function generateTeam(){
+    
   }
-}
-/*
-function init() {
-  inquirer.prompt(managerQuestionsArray)
-  .then((answers) =>{
-    answers['role']= 'Manager';
-    console.log(answers);
-    generatedTeamArray.push(answers)
-    promptMemberQuestions()
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-}
-*/
-// will need a function to generate the HTML and write to file
-function generateTeam(){
   
-}
-
-init()
-
-// looping array to generate the html
-for (let peopleObj of generatedTeamArray){
-  let name = peopleObj.name
-  let employeeId = peopleObj.employeeId
-}
-/* 
-const questions = async() => {
-  const managerAnswers = await inquirer
+  init()
+  
+  // looping array to generate the html
+  for (let peopleObj of generatedTeamArray){
+    let name = peopleObj.name
+    let employeeId = peopleObj.employeeId
+  }
+  // inquirer questions 
+  // questions for Manager on initial start
+  /*function promptMemberQuestions(){
+    inquirer.prompt(newMemberQuestion)
+    .then((answers) =>{
+      console.log(answers);
+      if (answers.role === 'Engineer') {
+        promptEngineerQuestions();
+      }
+      else if (answers.role === 'Intern') {
+        promptInternQuestions();
+      }
+      else if (answers.role === 'Finished'){
+        console.log(generatedTeamArray);
+        generateTeam();
+        return;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+  
+  // function to prompt engineer questions
+  function promptEngineerQuestions(){
+    inquirer.prompt(engineerQuestionsArray)
+    .then((answers) =>{
+      // add the role to the object
+      answers['role']= 'Manager';
+      console.log(answers);
+      promptMemberQuestions();
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+  
+  //function to prompt intern questions
+  function promptInternQuestions(){
+    inquirer.prompt(internQuestionsArray)
+    .then((answers) =>{
+      answers['role']= 'Intern';
+      console.log(answers);
+      generatedTeamArray.push(answers)
+      console.log(generatedTeamArray);
+      promptMemberQuestions();
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+  */
+  /* 
+  const questions = async() => {
+    const managerAnswers = await inquirer
     .prompt([
       {
         type: 'input',
